6fb159201e4a8dc4d2110da6ba082fe761c0a859
Nitsua28 <achan113@ucmerced.edu>
2023-03-20 14:24:15 -0700

    added properties files

--- src/test/java/com/revature/Project2_SouthParkComplaintApp_BackEnd/Project2SouthParkComplaintAppBackEndApplicationTests.java
+++ src/test/java/com/revature/Project2_SouthParkComplaintApp_BackEnd/Project2SouthParkComplaintAppBackEndApplicationTests.java
@@ -2,8 +2,10 @@ package com.revature.Project2_SouthParkComplaintApp_BackEnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:test.properties")
 class Project2SouthParkComplaintAppBackEndApplicationTests {
 
 	@Test
