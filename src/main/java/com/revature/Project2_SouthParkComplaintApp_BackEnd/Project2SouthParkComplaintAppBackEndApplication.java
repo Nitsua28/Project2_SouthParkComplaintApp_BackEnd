df6b41b98d755628bb97e26f8cac744b1018fdb4
Nitsua28 <achan113@ucmerced.edu>
2023-03-22 13:10:08 -0700

    edit url dependencies

--- src/main/java/com/revature/Project2_SouthParkComplaintApp_BackEnd/Project2SouthParkComplaintAppBackEndApplication.java
+++ src/main/java/com/revature/Project2_SouthParkComplaintApp_BackEnd/Project2SouthParkComplaintAppBackEndApplication.java
@@ -2,8 +2,10 @@ package com.revature.Project2_SouthParkComplaintApp_BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:prod.properties")
 public class Project2SouthParkComplaintAppBackEndApplication {
 
 	public static void main(String[] args) {
